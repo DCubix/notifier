dd28111a281ac7290e3394885de9dc396f0733c8
DCubix <diego95lopes@live.com>
2020-01-03 20:27:15 -0400

    Ajustes

--- src/com/diegolopes/WebSocketServer.java
+++ src/com/diegolopes/WebSocketServer.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
@@ -30,7 +31,7 @@ public class WebSocketServer {
 	public void onError(Throwable e){
 		e.printStackTrace();
 	}
-	
+		
 	public static void broadcast(String message) {
 		for (Session client : clients) {
 			if (client != null && client.isOpen()) {
